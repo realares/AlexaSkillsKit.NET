ec9c23258cc68ca99d866bdfa9b4ad286965d08d
realares <github_KJN572a@realares.de>
2017-07-15 17:14:43 +0200

    Update Copyright Notices

--- AlexaSkillsKit.Lib/Sdk.cs
+++ AlexaSkillsKit.Lib/Sdk.cs
@@ -1,4 +1,18 @@
-﻿//  Copyright 2015 Stefan Negritoiu (FreeBusy). See LICENSE file for more information.
+﻿/* 
+Original work Copyright (c) 2015 Stefan Negritoiu (FreeBusy) 
+Modified work Copyright 2017 Frank Kuchta
+
+The MIT License (MIT)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 using System;
 using Newtonsoft.Json;
@@ -21,7 +35,8 @@ namespace AlexaSkillsKit
         public const string SIGNATURE_KEY_TYPE = "RSA";
         public const int TIMESTAMP_TOLERANCE_SEC = 150;
 
-        public static JsonSerializerSettings DeserializationSettings = new JsonSerializerSettings {
+        public static JsonSerializerSettings DeserializationSettings = new JsonSerializerSettings
+        {
             MissingMemberHandling = MissingMemberHandling.Ignore,
             ContractResolver = new CamelCasePropertyNamesContractResolver(),
             Converters = new List<JsonConverter>
