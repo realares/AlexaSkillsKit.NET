e9453dc0e4454af5ddf0813568cc6e3686787a94
realares <github_KJN572a@realares.de>
2017-07-18 00:20:43 +0200

    Add licence Text

--- AlexaSkillsKit.Lib/UI/Speech/SsmlBuilder.cs
+++ AlexaSkillsKit.Lib/UI/Speech/SsmlBuilder.cs
@@ -1,6 +1,4 @@
-﻿/* 
-Original work Copyright (c) 2015 Stefan Negritoiu (FreeBusy) 
-Modified work Copyright 2017 Frank Kuchta
+﻿/* Copyright 2017 Frank Kuchta
 
 The MIT License (MIT)
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
